3f5f11d6a15cc3b74ca84e6fb0ffe3139c21e28d
Mickael Clavreul <mickael.clavreul@eseo.fr>
2026-02-18 00:08:48 +0100

    New french language

---
+++ lang/fr/block_coursefeedback.php
@@ -0,0 +1,169 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+// FRENCH
+
+$string['adminpage_html_allowhidinga'] = 'Autoriser à être caché';
+$string['adminpage_html_allowhidingb'] = 'Activé, les enseignants peuvent choisir de rendre visible ou de cacher le bloc.';
+$string['adminpage_html_defaultlanguagea'] = 'Langue par défaut';
+$string['adminpage_html_defaultlanguageb'] = "Si les questions ne sont pas disponibles dans la langue de l'utilisateur, les questions de la langue par défaut seront affichées. Assurez-vous de définir, au miminum, toutes les questions dans la langue par défaut!';
+$string['adminpage_html_enable_infobannera'] = 'Activé, la banière d'information est visible';
+$string['adminpage_html_enable_infobannerb'] = 'Désactivé, la banière d'information n'est pas visible.';
+$string['adminpage_html_fbactiveforcoursesa'] = 'Durée maximum depuis la début du cours';
+$string['adminpage_html_fbactiveforcoursesb'] = 'Définit la période à partir de laquelle le questionnaire est affiché.';
+$string['adminpage_html_globalenablea'] = 'Activé, le bloc apparaît dans tous les cours';
+$string['adminpage_html_globalenableb'] = 'Désactivé, le bloc est supprimé de tous les cours.';
+$string['adminpage_html_headingdefault_hiddena'] = 'Visibilité par défaut dans les cours';
+$string['adminpage_html_headingdefault_hiddenb'] = 'L'activation du bloc dans tous les cours ("global_enable") affiche les blocs par défaut.';
+$string['adminpage_html_default_hiddena'] = 'Masquer les blocs dans tous les cours'; 
+$string['adminpage_html_default_hiddenb'] = 'Activé, le bloc apparaît caché dans l'ensemble des cours, sauf si un enseignant le rend visible.'; 
+$string['adminpage_html_headinggeneral'] = 'Configuration générale';
+$string['adminpage_html_headinginfobannera'] = 'Configuration de la banière d'information';
+$string['adminpage_html_headinginfobannerb'] = 'La banière d'information, si activée, est affichée dans tous les cours où un questionnaire est en cours. L'activation n'est possible que si le bloc est affiché avec l'option "global_enable" et que la configuration du début du cours "since_coursestart" est conforme.';
+$string['adminpage_html_infobannera'] = 'Banière d'information';
+$string['adminpage_html_infobannerb'] = 'Entrez votre message, les filtres "multilanguage filter" sont autorisés.';
+$string['adminpage_html_notinstalled'] = '\'{$a}\' (non installé)';
+$string['adminpage_link_feedbackedit'] = 'créer/modifier un questionnaire';
+$string['adminpage_html_headingscale'] = 'Configuration de l'échelle d'évaluation';
+$string['adminpage_html_headingscale1'] = 'Classique';
+$string['adminpage_html_headingscale2'] = 'Numérique';
+$string['adminpage_html_headingscale3'] = 'Personnalisée';
+$string['adminpage_html_defaultscalea'] = 'Échelle d'évaluation';
+$string['adminpage_html_defaultscaleb'] = '"Classique" (défaut) utilise des émoticones et du texte, "Numérique" affiche des valeurs numériques (de 1 à n), et "Personnalisée" permet de définir sa propre échelle de valeurs (texte seulement).';
+$string['adminpage_html_scalenumbera'] = 'Nombre de valeurs';
+$string['adminpage_html_scalenumberb'] = 'Nombre de valeurs à afficher de 1 to n';
+$string['adminpage_html_scaletextsa'] = 'Valeurs personnalisées';
+$string['adminpage_html_scaletextsb'] = 'Une liste de valeurs séparées par des virgules.';
+$string['categorypath'] = 'Catégorie';
+$string['caution'] = 'Attention';
+$string['coursefeedback:addinstance'] = 'Ajouter ce bloc à l'accueil';
+$string['coursefeedback:download'] = 'Sauvegarder les résultats dans un fichier';
+$string['coursefeedback:evaluate'] = 'Évaluer les résultats';
+$string['coursefeedback:managefeedbacks'] = 'Modifier la configuration globale du bloc';
+$string['coursefeedback:myaddinstance'] = 'Ajouter le bloc sur la page "My home"';
+$string['coursefeedback:viewanswers'] = 'Analyse des résultats';
+$string['download_html_filename'] = 'Résultats';
+$string['download_thead_questions'] = 'Question';
+$string['eventviewed'] = 'Resultats affichés';
+$string['except_answer_exist'] = 'La réponse à cette question existe déjà';
+$string['except_block_duplicate'] = 'Il existe plus d'un bloc coursefeedback dans le cours';
+$string['except_block_hidden'] = 'Le bloc coursefeedback est masqué dans ce cours';
+$string['except_invalid_courseid'] = 'Identifiant de cours invalide';
+$string['except_invalid_questionid'] = 'Identifiant de question invalide';
+$string['except_no_question'] = 'Questionnaire ou question inconnu';
+$string['except_not_active'] = 'Question non activé';
+$string['except_wrong_questiontype'] = 'Type de réponse invalide';
+$string['form_area_questiontext'] = 'Editer le texte';
+$string['form_button_downloadfb'] = 'Télécharger les résultats pour un questionnaire';
+$string['form_button_downloadfb_essays'] = 'Télécharger les textes libres pour un questionnaire';
+$string['form_button_downloadqu'] = 'Télécharger les résultats pour une question';
+$string['form_copyof'] = 'Copier';
+$string['form_feedback_infotext'] = 'Texte de retour utilisateur';
+$string['form_feedback_infotext_help'] = 'Ce texte est utilisé pour informer l'utilisateur à propos du questionnaire. Il doit comporter un titre et toute information nécessaire.';
+$string['form_header_addlang'] = 'Rajouter un intitulé pour une autre langue';
+$string['form_header_confirm'] = 'Obligation de confirmer';
+$string['form_header_deleteanswers'] = 'Supprimer les réponses';
+$string['form_header_deletelang'] = 'Supprimer des langues';
+$string['form_header_editfeedback'] = 'Editer un questionnaire';
+$string['form_header_editquestion'] = 'Editer une question';
+$string['form_header_newfeedback'] = 'Nouveau questionnaire';
+$string['form_header_newquestion'] = 'Nouvelle question';
+$string['form_header_question'] = 'Question {$a}';
+$string['form_header_ranking'] = 'Configuration des statistiques';
+$string['form_html_currentlang'] = 'Vous éditez {$a}';
+$string['form_html_deleteanswerstext'] = 'Le questionnaire ne peut être modifié, des réponses ont été enregistrées. Vous pouvez soit supprimer les réponses, soit dupliquer le questionnaire pour le modifier.';
+$string['form_html_deleteanswerswarning'] = 'Les réponses seront définitivement supprimées, êtes-vous sûr ?';
+$string['form_html_nolangimplemented'] = 'Ce questionnaire n'a aucune langue définie.';
+$string['form_html_notextendable'] = 'Vous ne pouvez ajouter d'autre texte car aucune autre langue n'est disponible.';
+$string['form_notif_heading'] = 'Titre de la notification';
+$string['form_option_choose'] = 'Choisissez';
+$string['form_select_changepos'] = 'Déterminez la nouvelle position';
+$string['form_select_confirmyesno'] = 'Êtes-vous sûr de vouloir supprimer ?';
+$string['form_select_deleteanswers'] = 'Êtes-vous sûr de vouloir supprimer les réponses ?';
+$string['form_select_feedback'] = 'Sélectionnez un questionnaire';
+$string['form_select_newlang'] = 'Langue';
+$string['form_select_question'] = 'Sélectionnez une question';
+$string['form_select_unwantedlang'] = 'Sélectionnez une langue <br/><span style="font-size: x-small;">(choix multiples possibles)<span>';
+$string['infopage_headline_feedbackinfo'] = 'Information du questionnaire';
+$string['infopage_html_coursestartcountd'] = 'Le questionnaire est actif si plus de {$a} jours ont passé depuis le début du cours.';
+$string['infopage_link_feedbackinfo'] = 'Informations additionnelles';
+$string['notif_deactivate_howto'] = 'Vous pouvez désactiver le questionnaire en cachant le bloc "Course feedback".';
+$string['notif_emoji_bad'] = 'insatisfaisant';
+$string['notif_emoji_good'] = 'bien';
+$string['notif_emoji_neutral'] = 'passable';
+$string['notif_emoji_ok'] = 'assez bien';
+$string['notif_emoji_super'] = 'très bien';
+$string['notif_emoji_superbad'] = 'médiocre';
+$string['notif_feedbackactive'] = 'Un questionnaire est activé dans ce cours.';
+$string['notif_pleaseclick'] = 'Sélectionnez une valeur. Votre réponse est anonyme.';
+$string['notif_pleasewriteessay'] = 'Écrivez une réponse et cliquez sur "Envoyer". Votre réponse est anonyme.';
+$string['notif_question'] = 'Question ';
+$string['notif_submit'] = 'Envoyer';
+$string['notif_thankyou'] = 'Merci d'avoir répondu à cette enquête &#128522;';
+$string['numusers'] = 'Nombre de participants dans le cours';
+$string['page_headline_admin'] = 'Administration Course Feedback';
+$string['page_headline_listoffeedbacks'] = 'Liste de questionnaires';
+$string['page_headline_listofquestions'] = 'Questionnaire de \'{$a}\'';
+$string['page_html_activated'] = 'Questionnaire ({$a}) est maintenant actif.';
+$string['page_html_answersdeleted'] = 'Les réponses ont été supprimées.';
+$string['page_html_courserating'] = 'Classement du cours';
+$string['page_html_editallquestions'] = 'Appliquer à toutes les langues';
+$string['page_html_intronotifications'] = 'Ce questionnaire doit répondre aux conditions suivantes :';
+$string['page_html_nofeedbackactive'] = 'Questionnaire désactivé.';
+$string['page_html_noquestions'] = 'Aucune question disponible.';
+$string['page_html_norelations'] = 'Toutes les questions doivent avoir un texte dans au moins une langue commune.';
+$string['page_html_saveerr'] = 'Une erreur est survenue lors de l'enregistrement de la réponse.';
+$string['page_html_servedefaultlang'] = 'Toutes les questions doivent être définies dans la langue par défaut.';
+$string['page_html_totalanscountinfo'] = '{$a->anscount} réponses non vides au total, vous affichez la page {$a->page} sur {$a->totalpages}.';
+$string['page_html_viewintro'] = 'Analyse du questionnaire. Les résultats montrent le nombre de votes pour chaque valeur et la moyenne.';
+$string['page_html_viewnavbar'] = 'Analyse du questionnaire';
+$string['page_html_wasactive'] = 'a été activée précédemment';
+$string['page_link_backtoconfig'] = 'Retour à l'administration';
+$string['page_link_backtofeedbackview'] = 'Retour à l'affichage global';
+$string['page_link_deletelanguage'] = 'Supprimer une langue';
+$string['page_link_download'] = 'Sauvegarder les résulats dans un fichier {$a}';
+$string['page_link_newlanguage'] = 'Ajouter une langue';
+$string['page_link_newquestion'] = 'Ajouter une question';
+$string['page_link_newtemplate'] = 'Créer un questionnaire';
+$string['page_link_noquestion'] = 'Aucune question disponible.';
+$string['page_link_rankings'] = 'Résultats';
+$string['page_link_settings'] = 'Administration';
+$string['page_link_showlistofquestions'] = 'Éditez les questions';
+$string['page_link_use'] = 'Activer';
+$string['page_link_viewresults'] = 'Questions et résultats';
+$string['perm_header_editnotpermitted'] = 'Impossible de changer le questionnaire :';
+$string['perm_html_answersexists'] = 'Des réponses existent sur ce questionnaire.';
+$string['perm_html_danswerslink'] = 'Pour créer un nouveau questionnaire avec les mêmes questions, vous pouvez le <a href="admin.php?fid={$a}&mode=feedback&action=new">dupliquer</a> ou <href="admin.php?fid={$a}&mode=feedback&action=danswers">supprimer les réponses</a>.';
+$string['perm_html_duplicatelink'] = 'Pour créer un nouveau questionnaire avec les mêmes questions, vous pouvez le <a href="admin.php?fid={$a}&mode=feedback&action=new">copier</a> ou activer un autre questionnaire.';
+$string['perm_html_erroractive'] = 'Le questionnaire ne peut pas être changé.';
+$string['perm_html_wasactive'] = 'Le questionnaire ne peut pas être réactivé. Si vous voulez l'utiliser, dupliquez-le.';
+$string['pluginname'] = 'Course feedback';
+$string['qtype_empty_essayans'] = 'Il y a {$a} textes libres vides.';
+$string['questiontype'] = 'Type de question';
+$string['questiontype_essay'] = 'Texte libre';
+$string['questiontype_schoolgrades'] = 'Échelle de valeurs';
+$string['resultspage_headline'] = 'Liste de toutes les réponses pour le cours';
+$string['resultspage_howto'] = 'Cliquer sur le nom du questionnaire pour afficher les questions et les réponses.';
+$string['resultspage_nav_extension'] = 'Résulats du questionnaire';
+$string['resultspage_title'] = 'Résultats de l'enquête';
+$string['table_header_languages'] = 'Langues';
+$string['table_header_questions'] = 'Questions';
+$string['table_html_average'] = 'Moyenne';
+$string['table_html_nochoice'] = 'Abstentions';
+$string['table_html_nofeedback'] = 'Aucun questionnaire';
+$string['table_html_showanswers'] = 'Afficher les réponses';
+$string['table_html_undefinedlang'] = 'Pas de traduction. Langue \'{$a}\' indisponible.';
+$string['table_html_votes'] = ' Nombre de votes';
+$string['untitled'] = 'Sans titre';
